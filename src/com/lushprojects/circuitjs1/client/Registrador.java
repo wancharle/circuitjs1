21d8ae90379a9c1896f261830c7d8b6bed196350
Wancharle Quirino <wancharle.quirino@wowtec.com.br>
2017-11-23 21:12:48 -0200

    registrador de 8bits funcionando

--- src/com/lushprojects/circuitjs1/client/Registrador.java
+++ src/com/lushprojects/circuitjs1/client/Registrador.java
@@ -2,10 +2,11 @@ package com.lushprojects.circuitjs1.client;
 
 import java.lang.Math;
 public class Registrador extends ChipElm {
-    short data=0;//This has to be a short because there's no unsigned byte and it's screwing with my code
+    byte data=126;// 8 bits sinalizado em complemento de 2 vai de -128 para 127 
     int totalPinos;
     boolean clockstate=false;
-    
+    static int START_MEN_PIN = 3; //pino que comeca a armazenar bits  
+   
     boolean hasReset() {return false;}
     public Registrador(int xx, int yy) {super(xx, yy);totalPinos=8;}
     public Registrador(int xa, int ya, int xb, int yb, int f,
@@ -20,41 +21,59 @@ public class Registrador extends ChipElm {
 	    if(totalPinos==0)//forca inicializacao padrao
 		totalPinos=8;
 		    
-            sizeX = totalPinos+1;
+            sizeX = totalPinos+3;
             sizeY = 3;
             pins = new Pin[getPostCount()];
         
-            pins[0] = new Pin(1, SIDE_W, "D");
-            pins[1] = new Pin(2, SIDE_W, "");
+            pins[0] = new Pin(0, SIDE_W, "WR");
+            pins[1] = new Pin(1, SIDE_W, "");
             pins[1].clock=true;
+            pins[2] = new Pin(2, SIDE_W, "EN");
 	    
             String s=" ";
-            for (int i=1;i<=(totalPinos+1);i++){
-        	s = "I".concat(Integer.toString(totalPinos  - i));
-        	super.log(s);
-        	 pins[i+1] = new Pin(i, SIDE_N, s);
-        	 pins[i+1].output=true;	
+            int iside = 0;
+            int iq= 0;
+            int id= 0;
+            for (int i=START_MEN_PIN;i < (START_MEN_PIN+totalPinos);i++){
+                iside = i - START_MEN_PIN + 2;
+                iq = i;
+                id = i+totalPinos;
+
+                s = "Q".concat(Integer.toString(totalPinos  - iside +1));
+                pins[iq] = new Pin(iside, SIDE_S, s);
+                pins[iq].output=true;	
+
+                s = "D".concat(Integer.toString(totalPinos  - iside +1));
+                pins[id] = new Pin(iside, SIDE_N, s);
+
             }
 
     }
 
     int getPostCount() {
-	    return 2+totalPinos;
+	    return 3+2*totalPinos;
     }
 
     int getVoltageSourceCount() {return totalPinos;}
 
 
     void execute() {
+        boolean setBit = false;
+		if (pins[1].value && !lastClock){
+           if (pins[0].value == true) // write enable
+                for (int i=0;i< (totalPinos);i++){
+                    setBit = pins[START_MEN_PIN+totalPinos +i].value;
+                    data = setBit ? (byte)(data | (1 << i))
+                                  : (byte)(data & ~(1 << i));
+                }
+                
+        }     
 
-		if(pins[1].value&&!clockstate)
-		{
-		clockstate=true;
-		for (int i=2;i<= (totalPinos+1);i++)
-		    pins[i].value=false;
-		
-		}
-		if(!pins[1].value)clockstate=false;
+        if (pins[2].value == true)  // enable on
+            for (int i=0;i< (totalPinos);i++) pins[START_MEN_PIN+i].value = ((data & (byte)Math.pow(2,i)) != 0);
+        else
+            for (int i=0;i< (totalPinos);i++) pins[START_MEN_PIN+i].value = false;
+	    lastClock = pins[1].value;
 	}
 	
     int getDumpType() { return 1002; }
@@ -66,9 +85,7 @@ public class Registrador extends ChipElm {
             ei.choice = new Choice();
             ei.choice.add("4 Bits");
             ei.choice.add("8 Bits");
-            ei.choice.add("16 Bits");
-            ei.choice.add("32 Bits");
-            ei.choice.select(0); 
+            ei.choice.select((int)(choice-2)); 
             return ei;
         }
 
